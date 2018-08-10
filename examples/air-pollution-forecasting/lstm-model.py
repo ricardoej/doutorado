5d69f9dc790b7af6ad8093dbe74d95ac2a3ff112
Ricardo Emerson Julio <ricardoej@gmail.com>
2018-08-10 09:38:05 -0300

    lstm model to air pollution forecasting

--- examples/air-pollution-forecasting/lstm-model.py
+++ examples/air-pollution-forecasting/lstm-model.py
@@ -1,12 +1,16 @@
+from math import sqrt
+from numpy import concatenate
+from matplotlib import pyplot
+from pandas import read_csv
 from pandas import DataFrame
 from pandas import concat
-from pandas import read_csv
-from sklearn import preprocessing
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import mean_squared_error
 from keras.models import Sequential
-from keras.layers import LSTM, Dense, concatenate
-from matplotlib import pyplot
-
+from keras.layers import Dense
+from keras.layers import LSTM
+ 
 # convert series to supervised learning
 def series_to_supervised(data, n_in=1, n_out=1, dropnan=True):
 	n_vars = 1 if type(data) is list else data.shape[1]
@@ -35,20 +39,19 @@ def series_to_supervised(data, n_in=1, n_out=1, dropnan=True):
 dataset = read_csv('pollution.csv', header=0, index_col=0)
 values = dataset.values
 # integer encode direction
-encoder = preprocessing.LabelEncoder()
+encoder = LabelEncoder()
 values[:,4] = encoder.fit_transform(values[:,4])
 # ensure all data is float
 values = values.astype('float32')
 # normalize features
-scaler = preprocessing.MinMaxScaler(feature_range=(0, 1))
+scaler = MinMaxScaler(feature_range=(0, 1))
 scaled = scaler.fit_transform(values)
 # frame as supervised learning
 reframed = series_to_supervised(scaled, 1, 1)
 # drop columns we don't want to predict
 reframed.drop(reframed.columns[[9,10,11,12,13,14,15]], axis=1, inplace=True)
-# prints the first 5 rows of the transformed dataset
-#print(reframed.head())
-
+print(reframed.head())
+ 
 # split into train and test sets
 values = reframed.values
 n_train_hours = 365 * 24
@@ -60,8 +63,8 @@ test_X, test_y = test[:, :-1], test[:, -1]
 # reshape input to be 3D [samples, timesteps, features]
 train_X = train_X.reshape((train_X.shape[0], 1, train_X.shape[1]))
 test_X = test_X.reshape((test_X.shape[0], 1, test_X.shape[1]))
-#print(train_X.shape, train_y.shape, test_X.shape, test_y.shape)
-
+print(train_X.shape, train_y.shape, test_X.shape, test_y.shape)
+ 
 # design network
 model = Sequential()
 model.add(LSTM(50, input_shape=(train_X.shape[1], train_X.shape[2])))
@@ -70,11 +73,11 @@ model.compile(loss='mae', optimizer='adam')
 # fit network
 history = model.fit(train_X, train_y, epochs=50, batch_size=72, validation_data=(test_X, test_y), verbose=2, shuffle=False)
 # plot history
-#pyplot.plot(history.history['loss'], label='train')
-#pyplot.plot(history.history['val_loss'], label='test')
-#pyplot.legend()
-#pyplot.show()
-
+pyplot.plot(history.history['loss'], label='train')
+pyplot.plot(history.history['val_loss'], label='test')
+pyplot.legend()
+pyplot.show()
+ 
 # make a prediction
 yhat = model.predict(test_X)
 test_X = test_X.reshape((test_X.shape[0], test_X.shape[2]))
